5e36c96a41a7abef72bf21032cfe4ca4472daf21
Saraswathidharavath <dharavathsaraswathi512@gmail.com>
2025-03-15 22:10:34 +0530

    Add files via upload

--- goteo-live/db/tables/structure.sql
+++ goteo-live/db/tables/structure.sql
@@ -1,55 +1,16 @@
-CREATE TABLE IF NOT EXISTS node (
-  id varchar(50) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  active tinyint(1) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Nodos';
+CREATE TABLE IF NOT EXISTS purpose (
+  `text` varchar(50) NOT NULL,
+  purpose TEXT NOT NULL,
+  html tinyint(1) DEFAULT NULL COMMENT 'Si el texto lleva formato html',
+  `group` varchar(50) NOT NULL DEFAULT 'general' COMMENT 'Agrupacion de uso',
+PRIMARY KEY (`text`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Explicaci髇 del prop髎ito de los textos';
 
--- Alters
-ALTER TABLE `node` ADD `url` VARCHAR( 255 ) NOT NULL;
-ALTER TABLE `node`
-	ADD `subtitle` TEXT NULL ,
-	ADD `logo` INT( 20 ) UNSIGNED NULL ,
-	ADD `location` VARCHAR( 100 ) NULL ,
-	ADD `description` TEXT NULL ;
+-- Si no se puede pasar de nuevo el create, pasar el alter table
+ALTER TABLE `purpose` ADD `html` BOOLEAN NULL COMMENT 'Si el texto lleva formato html';
 
--- mail de contacto para impulsores
-ALTER TABLE `node` ADD `email` VARCHAR( 255 ) NOT NULL AFTER `name`;
+-- Agrupacion de usos de los textos
+ALTER TABLE `purpose` ADD `group` VARCHAR( 50 ) NOT NULL DEFAULT 'general' COMMENT 'Agrupacion de uso';
 
--- campo imagen a nombre archivo
-ALTER TABLE `node` CHANGE `logo` `logo` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Contiene nombre de archivo';
-
--- mail de contacto para impulsores
-ALTER TABLE `node` ADD `email` VARCHAR( 255 ) NOT NULL AFTER `name`;
-
--- perfiles sociales del nooo/canal
-ALTER TABLE `node`
-	  ADD `twitter`  tinytext COLLATE utf8_general_ci,
-  	ADD `facebook` tinytext COLLATE utf8_general_ci,
-    ADD `google` tinytext COLLATE utf8_general_ci,
-  	ADD `linkedin` tinytext COLLATE utf8_general_ci;
-
--- sello en proyectos
-ALTER TABLE `node` ADD `label` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Sello en proyectos';
-
--- image in channels home module
-ALTER TABLE `node` ADD `home_img` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Imagen para m贸dulo canales en home';
-
--- background color del m贸dulo de owner
-ALTER TABLE `node` ADD `owner_background` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Color de background m贸dulo owner';
-
--- asesor por defecto del canal
-ALTER TABLE `node` ADD `default_consultant` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Asesor por defecto para el proyecto';
-
--- limite de sponsors
-ALTER TABLE `node` ADD `sponsors_limit` INT( 2 ) DEFAULT NULL COMMENT 'N煤mero de sponsors permitidos para el canal';
-
--- contrains
-ALTER TABLE `node` CHANGE `default_consultant` `default_consultant` VARCHAR(50) CHARSET utf8 COLLATE utf8_general_ci NULL COMMENT 'Asesor por defecto para el proyecto', ADD FOREIGN KEY (`default_consultant`) REFERENCES `user`(`id`) ON UPDATE CASCADE ON DELETE SET NULL;
-
--- owner font color
-ALTER TABLE `node` ADD `owner_font_color` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Color de fuente m贸dulo owner';
-
--- owner social color grey or white
-ALTER TABLE `node` ADD `owner_social_color` VARCHAR( 255 ) NULL DEFAULT NULL COMMENT 'Color de iconos sociales m贸dulo owner';
-
+-- Textos mas largos en el proposito
+ALTER TABLE `purpose` CHANGE `purpose` `purpose` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL 
--- goteo-live/db/tables/structure_lang.sql
+++ goteo-live/db/tables/structure_lang.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS `node_lang` (
-`id` varchar(50) COLLATE utf8_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `icon_lang` (
+`id` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 `lang` varchar(2) NOT NULL,
-`subtitle` text COLLATE utf8_general_ci DEFAULT NULL,
-`description` text COLLATE utf8_general_ci DEFAULT NULL,
+`name` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+`description` TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
  UNIQUE KEY `id_lang` (`id`,`lang`)
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- pendiente de traducir
-ALTER TABLE `node_lang` ADD `pending` INT( 1 ) NULL DEFAULT '0' COMMENT 'Debe revisarse la traducción';
+ALTER TABLE `icon_lang` ADD `pending` INT( 1 ) NULL DEFAULT '0' COMMENT 'Debe revisarse la traducción';
